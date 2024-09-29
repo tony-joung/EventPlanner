d1d05f34ab650c531e69164be9a9c1a46327db0e
liamharvey112 <liamharvo@hotmail.com>
2024-09-29 17:45:42 +1000

    Added navigation to new page for edit view

---
+++ src/main/java/com/example/eventmanagementdemo/EventDetailsController.java
@@ -0,0 +1,57 @@
+package com.example.eventmanagementdemo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class EventDetailsController {
+
+    @FXML
+    public Button saveButton;
+    @FXML
+    public Button cancelButton;
+    @FXML
+    private ListView<Event> eventsListView;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField hostedByTextField;
+    @FXML
+    private TextField dateTextField;
+    @FXML
+    private TextField venueTextField;
+    @FXML
+    private TextField phoneTextField;
+
+    private final IEventDAO eventDAO;
+
+    public EventDetailsController() {
+        eventDAO = new EventDAOImpl();
+    }
+
+    /**
+     * Programmatically selects a event in the list view and
+     * updates the text fields with the event's information.
+     * @param event The event to select.
+     */
+    private void setEventDetails(Event event) {
+        eventsListView.getSelectionModel().select(event);
+        nameTextField.setText(event.getEventName());
+        hostedByTextField.setText(event.getHostedBy());
+        dateTextField.setText(event.getDate());
+        venueTextField.setText(event.getVenue());
+        phoneTextField.setText(event.getPhone());
+    }
+
+    public void onnSaveConfirm(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onCancelClick(ActionEvent actionEvent) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+}
